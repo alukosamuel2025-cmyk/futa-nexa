ababe7a4a953fc70c3684af26cbba2110993e3a4
alukosamuel2025-cmyk <alukosamuel2025@gmail.com>
2026-07-14 08:23:54 +0100

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAudB0J6VIVMLEnMdl7dLaENSlGHB1ULfY",
+  authDomain: "fortunate-hub.firebaseapp.com",
+  projectId: "fortunate-hub",
+  storageBucket: "fortunate-hub.firebasestorage.app",
+  messagingSenderId: "743010342137",
+  appId: "1:743010342137:web:a3640e993fc9bfe3b9fe28",
+  measurementId: "G-N69F538RKT",
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
